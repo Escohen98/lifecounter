a45e454b2d405070e4f68deb28f6cd72d817879a
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-01-28 16:49:10 -0800

    Created project

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Student User on 1/28/19.
+//  Copyright © 2019 University of Washington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
