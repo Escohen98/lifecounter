d6c2654f93b7820136c2835ecdf2209ce4aaf398
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-01-29 21:10:26 -0800

    Code works. Not just need to autoscale.

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -77,7 +77,7 @@ class ViewController: UIViewController {
     
     //Adds values. Sets the corresponding value to 0 if drops below that. Function used to prevent reference error.
         func addValues(_ label : Int, _ value : Int) {
-        if((values[label] + value) < 0) {
+        if((values[label] + value) <= 0) {
             values[label] = 0
         } else {
             values[label] += value
@@ -89,19 +89,19 @@ class ViewController: UIViewController {
         var player = 0
         if values[0] <= 0 {
             values[0] = 0
-            updateLabel(label: 0, value: 0)
+            updateLabel(label: 0, value: 0, lost: true)
             player = 1
         } else if values[1] <= 0 {
             values[1] = 0
-            updateLabel(label: 1, value: 0)
+            updateLabel(label: 1, value: 0, lost: true)
             player = 2
         } else if values[2] <= 0 {
             values[2] = 0
-            updateLabel(label: 2, value: 0)
+            updateLabel(label: 2, value: 0, lost: true)
             player = 3
         } else if values[3] <= 0 {
             values[3] = 0
-            updateLabel(label: 3, value: 0)
+            updateLabel(label: 3, value: 0, lost: true)
             player = 4
         }
         return player
