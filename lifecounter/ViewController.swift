cfabc61c03e1daff0b6408ab46d88af0590eae52
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-01-29 21:07:22 -0800

    Almost completely works.

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -11,11 +11,13 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-   // @IBOutlet weak var losingPlayer: UILabel? = nil //Losing Player
+   
+    @IBOutlet weak var losingPlayer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       // losingPlayer?.isHidden = false;
+        losingPlayer?.isHidden = true
     }
     @IBOutlet weak var lifeTotal1: UILabel! //Player 1
     @IBOutlet weak var lifeTotal2: UILabel! //Player 2
@@ -47,56 +49,66 @@ class ViewController: UIViewController {
      * Label refers to given player
      * value refers to button pressed.
      */
-    func updateLabel(label : Int, value: Int) {
+    func updateLabel(label : Int, value: Int, lost : Bool = false) {
+        addValues(label, value)
         switch label {
-        case 1:
-            values[0] += value
+        case 0:
             lifeTotal1?.text = String(values[0])
             break
-        case 2:
-            values[1] += value
+        case 1:
             lifeTotal2?.text = String(values[1])
             break
-        case 3:
-            values[2] += value
+        case 2:
             lifeTotal3?.text = String(values[2])
             break
-        case 4:
-            values[3] += value
+        case 3:
             lifeTotal4?.text = String(values[3])
             break
         default:
             break
         }
-        let loser = checkLoser()
-        if loser != 0 {
-           // losingPlayer?.text = "Player \(loser) Loses!"
+        if lost {
+            losingPlayer?.text = "Player \(label+1) Loses!"
+            losingPlayer?.isHidden = false
+        } else {
+            checkLoser()
         }
     }
-    //Checks if any lifeTotals are 0. If so, prints losing message.
+    
+    //Adds values. Sets the corresponding value to 0 if drops below that. Function used to prevent reference error.
+        func addValues(_ label : Int, _ value : Int) {
+        if((values[label] + value) < 0) {
+            values[label] = 0
+        } else {
+            values[label] += value
+        }
+    }
+    
+    //Checks if any lifeTotals are 0. If so, prints losing message. Returns losing player. Returns 0 if no one has lost.
     func checkLoser() -> Int {
         var player = 0
         if values[0] <= 0 {
             values[0] = 0
-            updateLabel(label: 1, value: 0)
+            updateLabel(label: 0, value: 0)
             player = 1
-        } else if lifeTotal2?.text == "0" {
+        } else if values[1] <= 0 {
             values[1] = 0
-            updateLabel(label: 2, value: 0)
+            updateLabel(label: 1, value: 0)
             player = 2
-        } else if lifeTotal3?.text == "0" {
+        } else if values[2] <= 0 {
             values[2] = 0
-            updateLabel(label: 3, value: 0)
+            updateLabel(label: 2, value: 0)
             player = 3
-        } else if lifeTotal4?.text == "0" {
+        } else if values[3] <= 0 {
             values[3] = 0
-            updateLabel(label: 4, value: 0)
+            updateLabel(label: 3, value: 0)
             player = 4
         }
         return player
     }
-    func getLabel(_ sender: UIButton) -> Int{
-        return 5 - sender.tag % 4
+    //Returns the label. (Player 1 is given the 0,3,7,11 tags)
+    func getLabel(_ sender: UIButton) -> Int {
+        return sender.tag % 4
     }
 }
 
