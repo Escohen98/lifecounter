127a347996eeb385adcbfff8dbaa4fac27be1f28
escohen98 <escohen98@gmail.com>
2019-01-30 17:23:58 -0800

    What a sexy mess.

--- lifecounter/player.swift
+++ lifecounter/player.swift
@@ -6,17 +6,37 @@
 //  Copyright © 2019 University of Washington. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-class player: UIViewController {
-   /* var player : UInt8;
-    init(player: UInt8) {
-        super.init()
-        self.player = player
+class player: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    //Defines how many cells to create
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }*/
+    //Initializes new cell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PlayersCell
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        players.delegate = self
+        players.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var players: UICollectionView!
+    
+    
+}
+
+//A player with a +, -, +5, -5 button, a name, and a lifetotal
+class PlayersCell: UICollectionViewCell{
+    @IBOutlet weak var playerName: UILabel!
+    @IBOutlet weak var lifeTotal: UILabel!
+    @IBOutlet weak var add1: UIButton!
+    @IBOutlet weak var aub1: UIButton!
+    @IBOutlet weak var add5: UIButton!
+    @IBOutlet weak var sub5: UIButton!
 }
