c1511b54acc84e02ed686c5f176328abb7ec4c2a
Student User <studentuser@D-10-19-237-147.dhcp4.washington.edu>
2019-01-31 17:17:14 -0800

    Caught up to assignment 1. SIGABRT error for unknown reason.

--- lifecounter/player.swift
+++ lifecounter/player.swift
@@ -24,6 +24,9 @@ class player: UIViewController, UICollectionViewDataSource, UICollectionViewDele
         cell.add5.tag = 5
         cell.sub5.tag = -5
 
+        cell.setLosingPlayer(player: losingPlayer)
+        cell.setPlayer(ID: indexPath.item + 1)
+        
         cell.add1.addTarget(self, action: #selector(cell.changeLife), for: .touchUpInside)
         cell.sub1.addTarget(self, action: #selector(cell.changeLife), for: .touchUpInside)
         cell.add5.addTarget(self, action: #selector(cell.changeLife), for: .touchUpInside)
@@ -38,7 +41,9 @@ class player: UIViewController, UICollectionViewDataSource, UICollectionViewDele
         players.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     @IBOutlet weak var players: UICollectionView!
+    @IBOutlet weak var losingPlayer: UILabel!
 }
 
 //A player with a +, -, +5, -5 button, a name, and a lifetotal
@@ -50,14 +55,30 @@ class PlayersCell: UICollectionViewCell{
     @IBOutlet weak var add5: UIButton!
     @IBOutlet weak var sub5: UIButton!
     
+    weak var losingPlayer: UILabel!
+    var playerID : Int = 0
+    
+    //Sets the UILabel of Losing Player.
+    func setLosingPlayer(player : UILabel!) {
+        losingPlayer = player
+    }
+    
+    //Sets the player id (1-8)
+    func setPlayer(ID : Int) {
+        playerID = ID
+    }
+    
+    //Adds the given amount of health to to health. Sets lifeTotal text to the health value.
     @IBAction func changeLife(_ sender: UIButton) {
         let health = UInt(Int(lifeTotal.text!)! + sender.tag)
         if(checkLoser(health: health)) {
+            losingPlayer.text = "Player \(playerID) loses!"
         } else {
             lifeTotal.text = "\(health)"
         }
     }
     
+    //Checks if health is 0. Returns true if yes, false otherwise.s
     func checkLoser(health : UInt) -> Bool{
         return health == 0
     }
