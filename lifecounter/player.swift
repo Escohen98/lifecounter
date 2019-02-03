cd5840ba1e679e83d59de8ec8883ded9a41324a0
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-02-02 17:40:03 -0800

    Added a stepper button to change the values of the +5/-5 buttons.

--- lifecounter/player.swift
+++ lifecounter/player.swift
@@ -11,12 +11,18 @@ import UIKit
 class player: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     private var playerCount = 4 //# of players
     private var lifeTotals : [UILabel?] = [] //collection of lifetotals
+    private var healthBtns : [Array<UIButton?>] = [] //2D array containing all buttons that modify a player's health and that aren't the +1, -1 btns
     //Defines how many cells to create
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return playerCount
     }
     
-    //Initializes new cell
+    /*
+     *Initializes new cell. Adds lifeTotals to lifeTotals array and
+     *designated health buttons to healthBtnsArray.
+     *Also sets the Losing Player and the playerID for each cell.
+     *Returns cell.
+     */
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PlayersCell
         cell.playerName.text = "Player \(indexPath.item + 1)"
@@ -24,7 +30,9 @@ class player: UIViewController, UICollectionViewDataSource, UICollectionViewDele
         cell.sub1.tag = -1
         cell.add5.tag = 5
         cell.sub5.tag = -5
+        
         lifeTotals.append(cell.lifeTotal)
+        healthBtns.append([cell.add5, cell.sub5])
         
         cell.setLosingPlayer(player: losingPlayer)
         cell.setPlayer(ID: indexPath.item + 1)
@@ -32,12 +40,12 @@ class player: UIViewController, UICollectionViewDataSource, UICollectionViewDele
         return cell
     }
     
-    override func viewDidLayoutSubviews() {
+   /* override func viewDidLayoutSubviews() {
         if let flowLayout = players.collectionViewLayout as? UICollectionViewFlowLayout {
-            let width = players.bounds.width / 3
+            let width = players.bounds.width / 1
             flowLayout.itemSize = CGSize(width: width, height: width)
         }
-    }
+    }*/
     
     //Sets the appDelegate and dataSource to the players class.
     override func viewDidLoad() {
@@ -72,13 +80,18 @@ class player: UIViewController, UICollectionViewDataSource, UICollectionViewDele
         return false
     }
     
-    @IBAction func changeIncrement(_ sender: UIButton) {
-        print(players.indexPathsForVisibleItems)
+    //Changes the magnitude of the increment values of the +5/-5 buttons. 1 shift per press. Also changes their title labels.
+    @IBAction func changeIncrement(_ sender: UIStepper) {
+        for btn in healthBtns {
+            btn[0]?.tag = Int(sender.value)
+            btn[0]!.setTitle("+\(btn[0]!.tag)", for: .normal)
+            btn[1]?.tag = -1*Int(sender.value)
+            btn[1]!.setTitle("\(btn[1]!.tag)", for: .normal)
+        }
     }
     
     @IBOutlet weak var players: UICollectionView! //Array of PlayerCell objects
     @IBOutlet weak var losingPlayer: UILabel! //The UILabel of the player that lost
-    @IBOutlet weak var changeValue: UIView!
 }
 
 //A player with a +, -, +5, -5 button, a name, and a lifetotal
@@ -135,11 +148,6 @@ class PlayersCell: UICollectionViewCell {
         
         let indexPath = NSIndexPath(row: sender.tag, section: 0)
         historySegue.performSegue(withIdentifier: "sendData", sender: indexPath)
-    }
-    
-    //Changes the increment value of the +5/-5 buttons.
-    func changeIncrement() {
-        
     }
     
     //Checks if health is 0. Returns true if yes, false otherwise.s
